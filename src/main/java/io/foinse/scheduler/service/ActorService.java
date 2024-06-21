02929331b4aeff5cc952380fead72f6e1c77eb94
Nathan Goss <nathan.goss@libertymutual.com>
2024-06-21 14:41:45 -0400

    get conflicts service

--- src/main/java/io/foinse/scheduler/service/ActorService.java
+++ src/main/java/io/foinse/scheduler/service/ActorService.java
@@ -1,9 +1,10 @@
 package io.foinse.scheduler.service;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -11,7 +12,9 @@ import io.foinse.scheduler.data.ActorRepository;
 import io.foinse.scheduler.data.CharacterRepository;
 import io.foinse.scheduler.entities.Actor;
 import io.foinse.scheduler.entities.Character;
+import io.foinse.scheduler.entities.ConflictInstance;
 import io.foinse.scheduler.model.AddActorRequest;
+import io.foinse.scheduler.util.ScheduleUtils;
 
 @Service
 public class ActorService {
@@ -25,7 +28,7 @@ public class ActorService {
     public Actor findById(String id) {
         Actor actor = actorRepository.findById(id);
 
-        if (actor == null ) {
+        if (actor == null) {
             throw new ResponseStatusException(NOT_FOUND);
         }
 
@@ -57,5 +60,16 @@ public class ActorService {
         }
 
         return actorRepository.delete(actor);
+    }
+
+    public List<ConflictInstance> getConflicts(String sid, String actorId, LocalDateTime start, LocalDateTime end) {
+        Actor actor = actorRepository.findById(actorId);
+
+        if (actor == null) {
+            throw new ResponseStatusException(NOT_FOUND);
+        }
+
+        return actor.getConflicts().stream()
+                .filter(c -> ScheduleUtils.checkIfConflictInRehearsalTimeRange(c, start, end)).toList();
     }
 }
